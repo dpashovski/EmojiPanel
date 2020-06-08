d84890a6f9b6f80f0d51cf376c83837b20c2cc6f
Dimitar Pashovski <pasha.bt@gmail.com>
2020-06-08 17:44:20 +0200

    Updated the latest build

--- src/emojis.js
+++ src/emojis.js
@@ -118,7 +118,7 @@ const Emojis = {
             emoji.parentNode.replaceChild(newElem,emoji);
         });
 
-        input.value = rawContent.innerHTML.replace(/&nbsp;/gi, ' ').replace(/<div><br><\/div>/gi, '').replace(/<p><br><\/p>/gi, '').trim();
+        input.value = rawContent.innerText.replace(/&nbsp;/gi, ' ').replace(/<div><br><\/div>/gi, '').replace(/<p><br><\/p>/gi, '').trim();
 
         //Trigger this events in order to work with Angular 1.6.5
         input.dispatchEvent(new Event('trigger'));
@@ -129,14 +129,8 @@ const Emojis = {
         let emojiRegex = ['(?:[\u2700-\u27bf]|(?:\\ud83c[\\udde6-\\uddff]){2}|[\\ud800-\\udbff][\\udc00-\\udfff]|[\u0023-\u0039]\\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\\ud83c[\\udd70-\\udd71]|\\ud83c[\\udd7e-\\udd7f]|\\ud83c\\udd8e|\\ud83c[\\udd91-\\udd9a]|\\ud83c[\\udde6-\\uddff]|[\\ud83c[\\ude01-\\ude02]|\\ud83c\\ude1a|\\ud83c\\ude2f|[\\ud83c[\\ude32-\\ude3a]|[\\ud83c[\\ude50-\\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\\ud83c\\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\\ud83c\\udccf|\u2934|\u2935|[\u2190-\u21ff])']
 
         options.editable_content.appendChild(newHtml);
-
-        var ranges = [
-            '\ud83c[\udf00-\udfff]', // U+1F300 to U+1F3FF
-            '\ud83d[\udc00-\ude4f]', // U+1F400 to U+1F64F
-            '\ud83d[\ude80-\udeff]'  // U+1F680 to U+1F6FF
-        ];
         options.editable_content.innerHTML = options.editable_content.innerHTML.replace(
-            new RegExp(ranges.join('|'), 'g'),
+            new RegExp(emojiRegex.join('|'), 'g'),
             '<span class="RichEditor-pictographImage" data-emoji="$&" contenteditable="false">$&</span>');
 
         Emojis.replaceEmojis(options);
@@ -223,13 +217,8 @@ const Emojis = {
         });
 
         // Split content into array, insert emoji at offset index
-
-        console.log(editable_content.textContent);
-
         let content = editable_content.textContent.split(editable_content.textContent);
         let inputContent = emojiAware.split(editable_content.textContent);
-
-        console.log(content);
 
         content.splice(offset, 0, emoji.char);
         content = content.join('');
